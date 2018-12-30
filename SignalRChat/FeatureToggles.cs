89b513209ad0b70b033e2422367583d2f0d0e26b
Fernando <fernandomatosaraujo@gmail.com>
2018-12-30 00:23:10 -0800

    First version of the layout

---
+++ SignalRChat/FeatureToggles.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace SignalRChat
+{
+    /// <summary>
+    /// This class is used to load all configuration from file configuration
+    /// </summary>
+    public class FeatureToggles
+    {
+        /// <summary>
+        /// Bellow must be assigned all parameters the must be load from appconfig.json 
+        /// </summary>
+        public bool EnableDeveloperExceptions { get; set; }
+    }
+}
--- SignalRChat/Startup.cs
+++ SignalRChat/Startup.cs
@@ -4,31 +4,74 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Hosting.Internal;
 using Microsoft.AspNetCore.Http;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 
 namespace SignalRChat
 {
     public class Startup
     {
+
+        
+        private readonly IConfigurationRoot configuration;
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public Startup(IHostingEnvironment env)
+        {
+            configuration = new ConfigurationBuilder()
+                .AddEnvironmentVariables()
+                .AddJsonFile($"{env.ContentRootPath}/appconfig.json")
+                .Build();
+
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //Allowing the resource of Dependency Injection
+            //Singleton for all application
+            services.AddSingleton<FeatureToggles>(x => new FeatureToggles
+            {
+                EnableDeveloperExceptions = configuration.GetValue<bool>("FeatureToggles:EnableDeveloperExceptions")
+            }); 
+
+            //Enabling MVC
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, FeatureToggles features)
         {
-            if (env.IsDevelopment())
+
+            //This show the default error page if the environment is not in development
+            app.UseExceptionHandler($"/View/Shared/Error.cshtml");
+
+            
+            //Check if the Environment is running under development 
+            if(features.EnableDeveloperExceptions)
             {
+                //Show the error for the developer
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
+            //Enabling the use of static files
+            app.UseStaticFiles();
+
+            //Using MVC
+            app.UseMvc(routes =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                routes.MapRoute("Default",
+                    "{controller=Home}/{action=Index}/{id?}");
             });
+
+            app.UseFileServer();
         }
     }
 }
